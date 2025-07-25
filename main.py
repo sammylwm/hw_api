d651dfd30137e4e987ab429ab91a814340ad15ff
sammy <lednevdosa@gmail.com>
2025-07-25 22:03:48 +0300

    nomore lifespan

--- main.py
+++ main.py
@@ -5,15 +5,10 @@ import uvicorn
 from api_v1 import router as api_router
 from api.users.views import router as users_router
 from core.config import settings
-from core.models import Base
-from core.models.db_helper import db_helper
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    async with db_helper.engine.begin() as conn:
-        await conn.run_sync(Base.metadata.create_all)
-
     yield
 
 
