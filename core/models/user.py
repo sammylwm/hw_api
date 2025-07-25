d59784a29949d02cf8ae8df29a78915051536a73
sammy <lednevdosa@gmail.com>
2025-07-25 20:58:14 +0300

    etap

---
+++ core/models/user.py
@@ -0,0 +1,10 @@
+from sqlalchemy.orm import Mapped
+
+from .base import Base
+
+
+class User(Base):
+    email: Mapped[str]
+    password: Mapped[str]
+    login_dn: Mapped[str]
+    password: Mapped[str]
