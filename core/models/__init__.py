63026387608333db9d2fc1fba366b2a96d8666ba
sammy <lednevdosa@gmail.com>
2025-07-26 12:51:31 +0300

    models classes

--- core/models/__init__.py
+++ core/models/__init__.py
@@ -3,9 +3,12 @@ __all__ = (
     "User",
     "DatabaseHelper",
     "db_helper",
+    "Classes"
 )
 
 
 from .base import Base
 from .user import User
 from .db_helper import db_helper, DatabaseHelper
+from .classes import Classes
+
