2ac779fac67d9e1d8816fbb668e36dd7d88a1bd2
sammy <lednevdosa@gmail.com>
2025-08-10 16:38:08 +0300

    fix docker compose

--- core/config.py
+++ core/config.py
@@ -7,7 +7,7 @@ BASE_DIR = Path(__file__).parent.parent
 class Setting(BaseSettings):
     api_v1_prefix: str = "/api/v1"
 
-    db_url: str = "postgresql+asyncpg://sammy:Leto2025@hwApp_pg:5432/hwApp"
+    db_url: str = "postgresql+asyncpg://sammy:Leto2025@hw_app_pg:5432/hwApp"
     db_echo: bool = False
 
 
