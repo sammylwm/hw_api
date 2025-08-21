ce8afd2b1bbb302eb74dc2f61b46df0facf67cb0
sammy <lednevdosa@gmail.com>
2025-08-21 14:09:54 +0300

    fix

--- alembic/env.py
+++ alembic/env.py
@@ -28,7 +28,7 @@ target_metadata = Base.metadata
 # can be acquired:
 # my_important_option = config.get_main_option("my_important_option")
 # ... etc.
-config.set_main_option("sqlalchemy.url", config_reader.config.DB_URL.get_secret_value())
+config.set_main_option("sqlalchemy.url", config_reader.config.DB_URL)
 
 def run_migrations_offline() -> None:
     """Run migrations in 'offline' mode.
--- config_reader.py
+++ config_reader.py
@@ -1,25 +1,16 @@
 from contextlib import asynccontextmanager
 from typing import AsyncGenerator
-from aiogram.client.default import DefaultBotProperties
-from aiogram.enums.parse_mode import ParseMode
-from pydantic import SecretStr
 from pydantic_settings import BaseSettings, SettingsConfigDict
 from aiogram import Bot, Dispatcher
 from fastapi import FastAPI
 
 
 class Config(BaseSettings):
-    BOT_TOKEN: SecretStr
-    WEBHOOK_URL: str
-    WEBAPP_URL: str
+    WEBHOOK_URL: str = "https://lednevs.ru/hw_api/api"
+    WEBAPP_URL: str = "https://lednevs.ru/hw_api"
     APP_HOST: str = "0.0.0.0"
     APP_PORT: int = "8000"
-    DB_URL: SecretStr
-
-    DB_USER: str
-    DB_PASSWORD: str
-    DB_NAME: str
-    DB_HOST: str
+    DB_URL: str = "postgresql+asyncpg://sammy:Dosya1009@db:5432/hwApp"
 
     model_config = SettingsConfigDict(
         env_file=".env",
@@ -29,19 +20,19 @@ class Config(BaseSettings):
 
 @asynccontextmanager
 async def lifespan(app: FastAPI) -> AsyncGenerator:
-    await bot.set_webhook(
-        url=f"{config.WEBHOOK_URL}/webhook",
-        allowed_updates=dp.resolve_used_update_types(),
-        drop_pending_updates=True,
-    )
+    # await bot.set_webhook(
+    #     url=f"{config.WEBHOOK_URL}/webhook",
+    #     allowed_updates=dp.resolve_used_update_types(),
+    #     drop_pending_updates=True,
+    # )
     yield
-    await bot.session.close()
+    # await bot.session.close()
 
 
 config = Config()
-bot = Bot(
-    config.BOT_TOKEN.get_secret_value(),
-    default=DefaultBotProperties(parse_mode=ParseMode.HTML),
-)
-dp = Dispatcher()
+# bot = Bot(
+#     config.BOT_TOKEN.get_secret_value(),
+#     default=DefaultBotProperties(parse_mode=ParseMode.HTML),
+# )
+# dp = Dispatcher()
 app = FastAPI(lifespan=lifespan)
