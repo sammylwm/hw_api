4c877f32646862b7800773908b35d8a48649ad44
sammy <lednevdosa@gmail.com>
2025-09-03 17:25:24 +0300

    fix views

--- api_v1/marks/views.py
+++ api_v1/marks/views.py
@@ -1,11 +1,15 @@
 from fastapi import APIRouter
 from api_v1.marks import parsing
 from pydantic import BaseModel
+
+
 class CheckData(BaseModel):
     login: str
     password: str
 
+
 router = APIRouter(tags=["Marks"])
+
 
 @router.post("/check/")
 async def check_lp(data: CheckData):
@@ -15,10 +19,12 @@ async def check_lp(data: CheckData):
     else:
         return 0
 
+
 @router.post("/")
 async def get_marks(data: CheckData):
     res = await parsing.parse(data.login, data.password)
     return res
+
 
 @router.post("/all/")
 async def get_all_marks(data: CheckData):
