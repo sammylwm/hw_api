478139435e3720b49d09ae8aea0f53c46e184d94
Sammy <lednevdosa@gmail.com>
2025-10-10 10:57:19 +0300

    schedule

--- test.py
+++
@@ -1,7 +0,0 @@
-import datetime
-import locale
-
-locale.setlocale(locale.LC_TIME, "ru_RU.UTF-8")
-
-date_obj = datetime.datetime.strptime("10.09.2025", "%d.%m.%Y")
-weekday = date_obj.strftime("%A").lower()
