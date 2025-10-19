b3c89c48766ef2ca23bb3d733ac23600e8b29d57
Sammy <lednevdosa@gmail.com>
2025-10-19 15:18:13 +0300

    not https

--- config_reader.py
+++ config_reader.py
@@ -3,10 +3,6 @@ from fastapi import FastAPI
 
 
 class Config(BaseSettings):
-    WEBHOOK_URL: str = "https://lednevs.ru/hw_api/api"
-    WEBAPP_URL: str = "https://lednevs.ru/hw_api"
-    APP_HOST: str = "0.0.0.0"
-    APP_PORT: int = "8000"
     DB_URL: str = "postgresql+asyncpg://sammy:Dosya1009@db:5432/hwApp"
 
 
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi.middleware.cors import CORSMiddleware
 from api_v1 import router as routers_api
-from config_reader import app, config
+from config_reader import app
 
 import uvicorn
 
@@ -18,6 +18,5 @@ app.include_router(routers_api)
 if __name__ == "__main__":
     uvicorn.run(
         app,
-        host=config.APP_HOST,
-        port=config.APP_PORT,
+        host="0.0.0.0",
     )
